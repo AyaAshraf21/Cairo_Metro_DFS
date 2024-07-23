43b8483b2299ee0af1db73a3d570ad53338c6018
AyaAshraf21 <aya851821@gmail.com>
2024-07-24 01:24:33 +0300

    solve transtion station problem

--- src/main/java/org/example/Controller.java
+++ src/main/java/org/example/Controller.java
@@ -8,7 +8,8 @@ public class Controller
 
     private static ArrayList<String> line1 = new ArrayList<>(Arrays.asList("New El-Marg", "El-Marg", "Ezbet El-Nakhl", "Ain Shams", "El-Matareyya", "Helmeyet El-Zaitoun", "Hadayeq El-Zaitoun", "Saray El-Qobba", "Hammamat El-Qobba", "Kobri El-Qobba", "Manshiet El Sadr", "EL-Demerdash", "Ghamra", "Al-Shohadaa", "Orabi", "Nasser", "Sadat", "Saad Zaghloul", "Al-Sayeda Zeinab", "El-Malek El-Saleh", "Mar Girgis", "El-Zahraa", "Dar El-Salam", "Hadayek El-Maadi", "Maadi", "Sakanat El-Maadi", "Tora El-Balad", "Kozzika", "Tora El-Asmant", "El-Maasara", "Hadayek Helwan", "Wadi Hof", "Helwan University", "Ain Helwan", "Helwan"));
     private static ArrayList<String> line2 = new ArrayList<>(Arrays.asList("Shubra El-Kheima", "Kolleyyet El-Zeraa", "Mezallat", "Khalafawy", "St. Teresa", "Rod El-Farag", "Masaraa", "Al-Shohadaa", "Attaba", "Mohamed Naguib", "Sadat", "Opera", "Dokki", "El Bohoth", "Cairo University", "Faisal", "Giza", "Omm El-Masryeen", "Sakiat Mekky", "El-Mounib"));
-    private static ArrayList<String> line3 = new ArrayList<>(Arrays.asList("Adly Mansour", "El Haykestep", "Omar Ibn El-Khattab", "Qobaa", "Hesham Barakat", "El-Nozha", "Nadi El-Shams", "Alf Maskan", "Heliopolis", "Haroun", "Al-Ahram", "Koleyet El-Banat", "Stadium", "Fair Zone", "Abbassiya", "Abdou Pasha", "El-Geish", "Bab El Shaaria", "Attaba", "Nasser", "Maspero", "Safaa Hegazy", "Kit Kat", "Tawfikeya", "Wadi El-Nile", "Gamaet El-Dowal Al-Arabiya", "Bulaq Al-Dakrour", "Cairo University"));
+    private static ArrayList<String> line3 = new ArrayList<>(Arrays.asList("Adly Mansour", "El Haykestep", "Omar Ibn El-Khattab", "Qobaa", "Hesham Barakat", "El-Nozha", "Nadi El-Shams", "Alf Maskan", "Heliopolis", "Haroun", "Al-Ahram", "Koleyet El-Banat", "Stadium", "Fair Zone", "Abbassiya", "Abdou Pasha", "El-Geish", "Bab El Shaaria", "Attaba", "Nasser", "Maspero", "Safaa Hegazy", "Kit Kat","Sudan","Imbaba","El-Bohy","El-Kawmeya Al-Arabiya","Ring Road","Rod El-Farag Axis"));
+    //private static ArrayList<String> line3part2 = new ArrayList<>(Arrays.asList("Adly Mansour","Kit Kat","Tawfikeya", "Wadi El-Nile", "Gamaet El-Dowal Al-Arabiya", "Bulaq Al-Dakrour", "Cairo University"));
 
     public static int totalPrice(int stationsNum)
     {
@@ -65,7 +66,50 @@ public class Controller
         {
             return line3;
         }
+//        else if(line3part2.contains(station))
+//        {
+//            return line3part2;
+//        }
         return new ArrayList<>();
+    }
+
+
+    private static ArrayList<String> getSharedLine(String transitionStation1, String transitionStation2)
+    {
+        if(line1.contains(transitionStation1) && line1.contains(transitionStation2))
+        {
+            return line1;
+        }
+        else if(line2.contains(transitionStation1) && line2.contains(transitionStation2))
+        {
+            return line2;
+        }
+        else if(line3.contains(transitionStation1) && line3.contains(transitionStation2))
+        {
+            return line3;
+        }
+        return  new ArrayList<>();
+    }
+
+
+    private static boolean isTransition(String transitionStation, ArrayList<String> path)
+    {
+        String stationBefore = path.get(path.indexOf(transitionStation)-1);
+        String stationAfter = path.get(path.indexOf(transitionStation)+1);
+
+        if(transtionStations.contains(stationBefore) && transtionStations.contains(stationAfter))
+        {
+            return !getSharedLine(stationBefore,transitionStation).contains(stationAfter);
+        }
+        else if(transtionStations.contains(stationBefore))
+        {
+            return !getValidLine(stationAfter).contains(stationBefore);
+        }
+        else if(transtionStations.contains(stationAfter) || (!transtionStations.contains(stationBefore) && !transtionStations.contains(stationAfter)))
+        {
+            return !getValidLine(stationBefore).contains(stationAfter);
+        }
+        return false;
     }
 
 
@@ -85,20 +129,19 @@ public class Controller
 
         for(String s : transtionStations)
         {
-            if(path.contains(s) && path.indexOf(s) != 0)
+            if(path.contains(s) && path.indexOf(s) != 0 && path.indexOf(s) != path.size()-1)
             {
-                ArrayList<String> lineBeforeTranstionStation = getValidLine(path.get(path.indexOf(s)-1));
-                ArrayList<String> lineAfterTranstionStation = getValidLine(path.get(path.indexOf(s)+1));
-                if(lineBeforeTranstionStation != lineAfterTranstionStation)
+                if(isTransition(s,path))
                 {
+                    ArrayList<String> lineShared = getSharedLine(s,path.get(path.indexOf(s)+1));
                     direction += (", Then transtion at ["+ s +"]  ");
-                    if(lineAfterTranstionStation.indexOf(path.get(path.indexOf(s)+1)) > lineAfterTranstionStation.indexOf(s))
+                    if(lineShared.indexOf(path.get(path.indexOf(s)+1)) > lineShared.indexOf(s))
                     {
-                        direction += ("["+lineAfterTranstionStation.get(lineAfterTranstionStation.size()-1) + "] Direction  ");
+                        direction += ("["+lineShared.get(lineShared.size()-1) + "] Direction  ");
                     }
                     else
                     {
-                        direction += ("["+lineAfterTranstionStation.get(0) + "] Direction  ");
+                        direction += ("["+lineShared.get(0) + "] Direction  ");
                     }
                 }
             }
